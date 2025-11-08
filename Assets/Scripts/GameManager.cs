eb60096774192e86ebf828298b4217030b3afbe5
Ruby <ruby.calmerman@gmail.com>
2025-11-09 00:14:24 +0100

    fragment counter

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "GameManager", menuName = "Scriptable Objects/GameManager")]
 public class GameManager : ScriptableObject
 {
+    public int fragmentsCollected = 0;
+    public int totalFragments = 5;
     public void SwitchScene(string sceneName)
     {
         UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
@@ -10,5 +12,9 @@ public class GameManager : ScriptableObject
     public void QuitGame()
     {
         Application.Quit();
+    }
+    public void CollectFragment()
+    {
+        fragmentsCollected++;
     }
 }
